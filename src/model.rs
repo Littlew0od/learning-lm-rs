27fb9564c14068e23eadcae138b11aa9507202ad
w0od <dingboning02@163.com>
2025-02-26 22:31:58 +0800

    mlp

--- src/model.rs
+++ src/model.rs
@@ -3,7 +3,7 @@ use std::vec;
 
 use crate::config::LlamaConfigJson;
 use crate::kvcache::KVCache;
-use crate::operators as OP;
+use crate::operators::{self as OP, matmul_transb, rms_norm, swiglu};
 use crate::params::LLamaParams;
 use crate::tensor::Tensor;
 use safetensors::SafeTensors;
@@ -167,7 +167,23 @@ fn mlp(
     rms_w: &Tensor<f32>,
     eps: f32,
 ) {
-    todo!("Implement mlp");
+    // hidden = rms_norm(residual)
+    rms_norm(hidden_states, residual, rms_w, eps);
+
+    // gate = hidden @ gate_weight.T
+    matmul_transb(gate, 0., hidden_states, w_gate, 1.);
+
+    // up = hidden @ up_weight.T
+    matmul_transb(up, 0., hidden_states, w_up, 1.);
+
+    // act = gate * sigmoid(gate) * up ## SwiGLU
+    swiglu(up, gate);
+
+    // output = act @ down_weight.T
+    // residual = output + residual
+    matmul_transb(residual, 1., up, w_down, 1.);
+    
+    // todo!("Implement mlp");
 }
 
 #[test]
